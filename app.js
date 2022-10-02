4b2164ff340a86de3ac0698d70268de4d6a21dd2
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-10-02 20:30:29 +0900

    Add fs details comment

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+const fs = require('fs'); // fsモジュールを使用
+const readline = require('readline');
+const rs = fs.createReadStream('./popu-pref.csv'); //ファイルストリームを生成 fpみたいなもの
+const rl = readline.createInterface({ input: rs });
+rl.on('line', lineString => {
+  console.log(lineString);
+});
